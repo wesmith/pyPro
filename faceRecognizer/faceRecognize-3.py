992b60bb7734e89a78e51d090c86c7ce12243ed6
wesmith <smiwarsky@gmail.com>
2020-12-19 04:49:26 -0700

    replaced compare_faces() with face_distance(): getting 100% ID on McWhorter test set: better than before

--- faceRecognizer/faceRecognize-3.py
+++ faceRecognizer/faceRecognize-3.py
@@ -43,13 +43,25 @@ for root, dirs, files in os.walk(unkn_dir):
         testImage = cv2.cvtColor(testImage, cv2.COLOR_RGB2BGR)
 
         for (top, right, bottom, left), face_encoding in zip(facePositions, allEncodings):
-            name = 'unknown'
-            matches = face_recognition.compare_faces(Encodings, face_encoding)
-            if True in matches:
-                first_match_index = matches.index(True)
-                name = Names[first_match_index]
+
+            name  = 'unknown'
+
+            # WS note: compare_faces uses a default distance of 0.6 to be considered a match
+            #matches = face_recognition.compare_faces(Encodings, face_encoding)
+
+            # WS mod: examine distances to training faces: better to choose smallest distance
+            #         than to randomly take first True match from the list
+            distances = face_recognition.face_distance(Encodings, face_encoding)
+            min_dist  = distances.min()
+
+            if min_dist < 0.6: # considered a match if true
+            #if True in matches:
+                #first_match_index = matches.index(True)
+                #name  = Names[first_match_index]
+                index_of_min = distances.argmin()  # WS mod: choose smallest distance
+                name  = '{}: {:2.2f}'.format(Names[index_of_min], min_dist)
             cv2.rectangle(testImage, (left, top), (right, bottom), (0, 0, 255), 2)
-            cv2.putText(testImage, name, (left, top - 6), font, 1, (0, 255, 255), 1)
+            cv2.putText(testImage, name, (left, top - 6), font, 1, (100, 255, 255), 1)
 
         cv2.imshow('window', testImage)
         cv2.moveWindow('window', 0, 0)
